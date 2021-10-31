9a863b13d7f7c4267c3678827bf50d3715c47840
Arne Gideon <arne.gideon@gmail.com>
2021-10-31 13:06:44 +0100

    Visualizer added colors and edge labels

--- src/eval/visualizer.py
+++ src/eval/visualizer.py
@@ -4,18 +4,19 @@
 
 import matplotlib.pyplot as plt
 import networkx as nx
+import matplotlib as mpl
 
 
 def show_network_graph(tags: dict):
-    # TODO: Color vertices and edges, Fix Label positions, Label edges.
     vertices = {}
     edges = {}
+    entities = ["place", "location", "spatial_entity", "nonmotion_event", "path"]
 
     # Collect all potential vertices
-    for n in ["place", "location", "spatial_entity", "nonmotion_event", "path"]:
+    for n in entities:
         try:
             for entity in tags[n]:
-                vertices[entity["id"]] = {"label": entity["text"], "type": entity}
+                vertices[entity["id"]] = {"label": entity["text"], "type": n}
         except KeyError as e:
             pass
 
@@ -23,7 +24,7 @@ def show_network_graph(tags: dict):
     for n in ["qslink", "olink"]:
         try:
             for e in tags[n]:
-                edges[e["id"]] = {"fromid": e["fromid"], "toid": e["toid"], "label": e["reltype"]}
+                edges[e["id"]] = {"fromid": e["fromid"], "toid": e["toid"], "label": e["reltype"], "type": n}
         except KeyError as e:
             pass
 
@@ -45,23 +46,40 @@ def show_network_graph(tags: dict):
     # Create Graph and ad edges that are in vertices
     g = nx.Graph()
     # Filter out edges containing stuff we don't want, e.g. motion tags
-    g.add_edges_from([(edges[x]["fromid"], edges[x]["toid"], {"weight": 0.25}) for x in edges.keys() if
-                      edges[x]["fromid"] in vertices.keys() and edges[x]["toid"] in vertices.keys()])
+    edges = [(edges[x]["fromid"], edges[x]["toid"],
+              {"weight": 0.18, "label": edges[x]["label"], "type": edges[x]["type"]})
+             for x in edges.keys() if
+             edges[x]["fromid"] in vertices.keys() and edges[x]["toid"] in vertices.keys()]
+    g.add_edges_from(edges)
 
-    pos = nx.spring_layout(g)  # positions for all nodes
+    pos = nx.kamada_kawai_layout(g, pos=nx.spring_layout(g, seed=5, iterations=500))  # positions for all nodes
 
     # nodes
-    options = {"edgecolors": "tab:gray", "node_size": 50, "alpha": 0.9}
-    nx.draw_networkx_nodes(g, pos, node_color="tab:red", **options)
+    # Determine colors for nodes
+    color_lookup = {k: entities.index(vertices[k]["type"]) for k in list(g.nodes)}
+    low, *_, high = sorted(color_lookup.values())
+    norm = mpl.colors.Normalize(vmin=low, vmax=high, clip=True)
+    mapper = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.summer)
+
+    options = {"edgecolors": "tab:gray",
+               "node_size": 100,
+               "alpha": 0.9,
+               "node_color": [mapper.to_rgba(i) for i in color_lookup.values()]}
+    nx.draw_networkx_nodes(g, pos, **options)
+
     # edges
-    nx.draw_networkx_edges(g, pos, width=1.0, alpha=0.5)
+    nx.draw_networkx_edges(g, pos, width=1.0, alpha=0.5,
+                           edge_color=["r" if x[2]["type"] == "qslink" else "b" for x in edges])
+
     # labels
-    labels = {x: vertices[x]["label"] for x in g.nodes}
-    nx.draw_networkx_labels(g, pos, labels, font_size=4, font_color="black")
+    nx.draw_networkx_labels(g, pos, {x: vertices[x]["label"] for x in g.nodes}, font_size=4, font_color="black")
+    nx.draw_networkx_edge_labels(g, pos,
+                                 edge_labels={(x[0], x[1]): x[2]["label"] for x in edges},
+                                 font_size=2)
 
     plt.tight_layout()
     plt.axis("off")
-    plt.savefig("test.png", dpi=300)
+    plt.savefig("test.png", dpi=400)
 
 
 def main():
